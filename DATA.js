b2e818b7ab12b2ccadd95ffea4785d05a6085feb
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 00:14:09 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "bd8ea690-0a62-409a-a2db-906503a7b71a",
+    "home_feed": [
+        {
+            "id": "ac215f4f-11e9-4834-bc05-64ea5aca648e",
+            "artist_name": "total",
+            "home_carousel": [
+                {
+                    "id": "1fdde86b-9040-4ba6-9e46-81b5185191d7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png"
+                },
+                {
+                    "id": "f7e850d8-8e37-47b5-be46-f29c504250e8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                },
+                {
+                    "id": "d40f4b20-d38c-4f52-9166-923e5000ee80",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/16.png"
+                },
+                {
+                    "id": "1854b007-4060-4771-8322-4d7b28206059",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/226.png"
+                },
+                {
+                    "id": "ede56928-f8cb-4ecf-89bf-dfa483cec6a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/133.png"
+                }
+            ],
+            "art_caption": "I",
+            "art_description": "Today list let though. May suddenly likely be marriage scientist here. Require eat record program picture campaign."
+        },
+        {
+            "id": "52c805ab-c578-4897-b141-a906ddfa6c28",
+            "artist_name": "billion",
+            "home_carousel": [
+                {
+                    "id": "4e1e6eb4-9497-46d0-8b07-20726585b609",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "c9bb0e42-bced-41a8-a0d5-f2ccab4effa9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/237.png"
+                },
+                {
+                    "id": "a0c3ab33-5294-4666-9915-123dc5863a4d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/273.png"
+                },
+                {
+                    "id": "1d362e88-94ac-4554-ba24-0b94b59503fa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png"
+                },
+                {
+                    "id": "f97e97c1-9800-4693-bcfd-70bad5ebbe53",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                }
+            ],
+            "art_caption": "song",
+            "art_description": "Long see far television treatment. Send station near."
+        },
+        {
+            "id": "f76a28db-fc8a-4b1e-b466-11ed7fb1bcda",
+            "artist_name": "alone",
+            "home_carousel": [
+                {
+                    "id": "0be7130e-4aef-406a-a3ea-6fa3c9fab41d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                },
+                {
+                    "id": "da09347a-1330-4443-997a-946ee1d7e26c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "821eb492-fd46-4f36-b7f8-338ddf707231",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "d93177f7-840e-43ac-afc8-dfc871a86512",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/46.png"
+                },
+                {
+                    "id": "8b52d776-1c16-4933-8875-ced368650ecb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png"
+                }
+            ],
+            "art_caption": "high",
+            "art_description": "Follow they song get result. Pretty food find test lawyer structure store."
+        },
+        {
+            "id": "b6d290da-edf0-48fc-ba6f-5962a35ddea9",
+            "artist_name": "pay",
+            "home_carousel": [
+                {
+                    "id": "194981b4-e3a5-40ad-bc71-4210c4f0631a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png"
+                },
+                {
+                    "id": "54feedc9-a662-49fa-9bf2-ecd912ba0405",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/303.png"
+                },
+                {
+                    "id": "1d4cf308-a3b7-43c7-8a65-6b4b2e2f32bd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png"
+                },
+                {
+                    "id": "7d954086-1b93-47e8-83fd-3f1c34bfe024",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/272.png"
+                },
+                {
+                    "id": "91e3397c-e18e-4ead-a2de-fe2bbc2f2c3e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+                }
+            ],
+            "art_caption": "consumer",
+            "art_description": "Issue admit arrive this establish kid spring. One skin age support possible since. Including them wait color."
+        },
+        {
+            "id": "881dfe39-1999-496a-a82f-02ab958de55e",
+            "artist_name": "add",
+            "home_carousel": [
+                {
+                    "id": "3d1bc5b4-9422-467b-9ab2-e9b019a53153",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "4a6b43de-1c03-4af5-902f-c5d19f873dcd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png"
+                },
+                {
+                    "id": "443fc4e5-401f-485d-8ee4-cb74f68b3c30",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/157.png"
+                },
+                {
+                    "id": "2ab7c568-537a-4439-86cd-1f3b1a154543",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png"
+                },
+                {
+                    "id": "22560993-32e3-4f24-871e-e7e10cc8d94c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+                }
+            ],
+            "art_caption": "movie",
+            "art_description": "Father themselves newspaper community continue. Stuff specific media several to produce another. Establish work everything together spring store."
+        }
+    ]
+}
